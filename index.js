93dd91a5dad7de5246a539df060bd51815ef6386
Brian Burkard <brianburkard@gmail.com>
2019-08-25 19:39:29 -0400

    Restful cats

---
+++ index.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const app = express();
+
+// Mongoose and MongoDB
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+var db = mongoose.connection;
+
+var catSchema = new mongoose.Schema({
+    name: String
+});
+
+catSchema.methods.speak = function () {
+    var greeting = this.name ? `Meow, name is ${this.name}` : "I don't have a name";
+    console.log(greeting);
+}
+
+var Cat = mongoose.model('Cat', catSchema);
+
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function() {
+    console.log('mongoose connected!');
+    //var zuzu = new Cat({name: 'Zuzu'});
+
+    // zuzu.save(function(err,zuzu){
+    //     if (err) console.error(err);
+    //     zuzu.speak();
+    // });
+
+    //var hobbes = new Cat({name: 'Hobbes', color: 'Orange'});
+    // hobbes.save(function(err,zuzu){
+    //     if (err) console.error(err);
+    //     hobbes.speak();
+    // });
+
+    // Cat.find((err, cats)=>{
+    //     if (err) console.error(err);
+    //     console.log(cats);
+    // });
+
+
+});
+//////////////////////////////////////
+
+// Todo replace with mongodb database,
+
+
+app.get('/api/:collection', function(req,res) {
+    //console.log(db);
+    console.log(req.params.collection);
+    if(req.params.collection == "Cats"){
+        Cat.find(function(err, cats){
+            if(err) console.log('error: ' + err);
+            console.log(cats);
+            res.send(cats);
+        });
+    }
+});
+
+
+app.get('/api/:collection/:name', function(req,res) {
+
+    const collection = req.params.collection;
+    const name = req.params.name;
+
+    if(collection =='Cat');
+    var query = Cat.find({'name':name });
+    query.select('name');
+    //query.limit(1);
+    //query.sort({name: -1});
+    query.exec( function(err, cat){
+        if(err) return console.log('error: ' + err);
+        res.send(cat);
+    });
+
+});
+
+
+
+
+
+
+
+// Port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
+
+// const courses = [
+//     {'id':1, 'name': 'course1'},
+//     {'id':2, 'name': 'course2'},
+//     {'id':3, 'name': 'course3'},
+// ];
+
+
+// // replace with Mongoose crud operations.
+
+// app.get('/api/courses', (req, res) => {
+//     //request from db
+//     res.send(courses);
+// });
+
+// app.get('/api/courses/:id', (req, res) => {
+//     const course = courses.find(c => c['id'] === Number.parseInt(req.params.id));
+//     if(!course) return res.status(404).send('The course with the given ID was not found.');// 404
+//     console.log(course);
+//     res.send(course);
+//     //res.send(courses[req.params.id]);
+// });
+
+// app.put('/api/courses/:id', (req, res) => {
+//     const course = courses.find(c => c['id'] === Number.parseInt(req.params.id));
+//     if(!course) return res.status(404).send('The course with the given ID was not found.');// 404
+    
+//     courses.course = req.body;
+//     res.send(req.body);
+    
+// });
+
+// app.post('/api/courses/', (req, res) => {
+//     courses.push(req.body);
+//     res.send(req.body);
+
+// });
+
+
